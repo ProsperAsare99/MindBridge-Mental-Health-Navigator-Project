4c5dd495000b3b5d23f80aacc9b12e603a499a2f
Asare Prosper Shaibu <asareprosper143@gmail.com>
2026-03-17 12:48:59 +0000

    feat: Add Pexels to allowed image remote patterns in Next.js config.

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "img.youtube.com" },
+      { protocol: "https", hostname: "images.pexels.com" },
       { protocol: "http", hostname: "localhost" },
       { protocol: "http", hostname: "127.0.0.1" }
     ],
