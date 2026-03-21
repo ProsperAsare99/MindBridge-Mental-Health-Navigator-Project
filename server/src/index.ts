750bde8588ad2bfe203956197b81505f85f07cfa
Asare Prosper Shaibu <asareprosper143@gmail.com>
2026-03-21 20:57:46 +0000

    feat: Initialize server entry point to load environment variables and start the application.

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,10 +3,10 @@ dotenv.config();
 
 import app from './app';
 
-const PORT = process.env.PORT || 5000;
+const PORT = parseInt(process.env.PORT || '5000');
 
 // Server entry point - Syncing innovative features.
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '127.0.0.1', () => {
+    console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
 
