a21f39ca568ac660615d29950bfaf42cbad025cb
Asare Prosper Shaibu <asareprosper143@gmail.com>
2026-03-24 13:48:19 +0000

    feat: Set up mobile API service with an authentication interceptor and initialize the server with circle management.

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import { initializeCircles } from './controllers/socialController';
 const PORT = parseInt(process.env.PORT || '5000');
 
 // Server entry point - Syncing innovative features.
-const HOST = '0.0.0.0';
+const HOST = '10.44.20.111';
 app.listen(PORT, HOST, async () => {
     console.log(`Server is running on http://${HOST}:${PORT}`);
     await initializeCircles();
